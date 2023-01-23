45026ee05a490740f8b3fee844d362f43244e295
Christopher Fink <christopherfink@Christophers-MacBook-Pro.local>
2023-01-22 20:41:22 -0500

    adding the weekly assignment

--- 2023-1-19.R
+++ 2023-1-19.R
@@ -47,3 +47,67 @@ STR(dataSub)
 
 orderedIris <- iris[order(iris$Petal.Length),]
 head(orderedIris)
+
+
+#### Functions in R
+
+# everythinh in R is a function
+sum(3, 2) # sum
+
+# user definded functions
+
+##functionName <- function(argX=defaultX, argY=defaultY){
+## Curly brackets starts the body of the function
+### Lines of R code ### and notes
+### create local variables (only 'visible' to R within the funciton)
+#argX <- c(IDK)
+## return(z)
+
+myFunc <- function(a=3,b=4){
+  z <- a + b
+  return(z)
+}
+myFunc() # runs defaults
+myFunc(a = 100, b=10)
+
+
+#### Multiple return statements
+
+# Function: HardyWeinberg
+# input: all allele frequency p (0,1)
+#output: p and the frequencies of 3 genotypes AA AB BB
+
+HardyWeinberg <- function(p = runif(1)){
+  if(p > 1.0 | p < 0.0){
+    return("Function failure: p must be between 0 and 1")
+  }
+  q <- 1 - p
+  fAA <- p^2
+  fAB <- 2*p*q
+  fBB <- q^2
+  vecOut <- signif(c(p=p, AA=fAA, AB= fAB, BB = fBB), digits = 3)
+  return(vecOut)
+}
+
+HardyWeinberg()
+
+
+### Create a complex default value
+
+# FUNCTION: fitlinear2
+# fits a simple regression line
+# input: numeric list (p) of predictor (x) and response (y)
+# outputs: slope and p-value
+
+fitlinear2 <- function(p=NULL){
+  if(is.null(p)){
+    p <- list(x=runif(20), y=runif(20))
+  }
+  myMod <- lm(p$x~p$y)
+  myOut <- c(slope=summary(myMod)$coefficients[2,1], pValue = summary(myMod)$coefficients[2,4])
+  plot(x=p$x, y=p$y)
+  return(myOut)
+}
+
+fitlinear2(p=myPars)
+myPars <- list(x=1:10, y=runif(10))

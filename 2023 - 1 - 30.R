c2a3da9e8996e8dc02d66f2d0110bd14b12ec1b1
Christopher Fink <christopherfink@christophersmbp.middlebury.edu>
2023-01-31 13:02:09 -0500

    adding assignment 9

--- 2023 - 1 - 30.R
+++ 2023 - 1 - 30.R
@@ -101,3 +101,89 @@ countDataLong <- countData %>%
   as.tibble() %>% 
   mutate(Species = c(rownames(countData))) %>% 
   pivot_longer(cols = Males:Females, names_to = "Sex", values_to = "Count")
+
+ggplot(data = countDataLong, mapping = aes(x=Species, y=Count, fill = Sex)) + 
+  geom_bar(stat="identity", position = "dodge") +
+  scale_fill_manual(values=c("darkslategray", "midnightblue"))
+
+
+
+############################################################################
+# Control Structure
+
+# if/else and if statements
+
+### if statement:: if(condition) {expression 1}
+### if (condition) {expression} else {exression 2}
+### if (condition 1) {expression 1} else if (condition 2) {expression 2} else {expression 3}
+### if any final unspecified else captures the rest of conditions
+# else must appear on the same line as the expression
+# use it for single values
+
+z <- signif(runif(1), digits=2)
+if (z > 0.8) {cat(z, "is a large number", "\n")} else 
+  if (z < 0.2) {cat(z, "is a small number", "\n")} else 
+  {cat(z, "is a number of typical size", "\n")
+    cat("z^2 =", z^2, "\n")}
+
+## ifelse to fill vectors
+### ifelse(condition, yes, no)
+
+#### insect population where females lay 10.2 eggs on average, follows a Poisson distribution (discrete probability distribution showing the likely number of times an event will occur). 35% parasitism where no eggs are laid. make a distribution for 1000 individuals
+
+tester <- runif(1000)
+eggs <- ifelse(tester>0.35, rpois(n=1000, lambda = 10.2), 0)
+hist(eggs)
+
+pVals <- runif(1000)
+z <- ifelse(pVals <= 0.025, "lowerTail", "nonSig")
+z
+
+
+# for loops
+# workhorse function for doing all computer languages
+### controversial in R
+## - often necessary (vectorization in R)
+#### very slow with certain operation
+### family of apply functions
+
+
+## anatomy of for loop
+## for(variable in a seq){start middle and end of for loop
+#}
+# var is a counter variable that holds the current value of the loop (i, j, k)
+# sequence is an integer vector that defines start/end of loop
+
+for(i in 1:5){
+  cat("stuck in a loop", i, "\n")
+  cat(3+2, "\n")
+  cat(3+i, "\n")
+}
+
+
+my_dogs <- c("chow", "akita", "malamute", "husky", "samoyed")
+for(i in 1:length(my_dogs)){
+  cat("i=", i, "my_dogs[i]=", my_dogs[i], "\n")
+}
+
+
+#### use double for loops
+
+m <- matrix(round(runif(20), digits=2), nrow = 5)
+
+for (i in 1:nrow(m)) {
+  m[i,] <- m[i, ] + i
+}
+m
+
+for (j in 1:ncol(m)){
+  m[,j] <- m[,j] +j
+}
+m
+
+for(i in 1:nrow(m)){
+  for(j in 1:ncol(m)){
+    m[i,j] <- m[i,j] + i + j
+  }
+}  
+m
